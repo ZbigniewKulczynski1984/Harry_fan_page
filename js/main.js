ec387f60a8a9684214580a4f9be00eecde6cb5cb
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-11 19:19:10 +0100

    code added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const sliderBox = document.querySelector('.slider-box')
+const leftBtn = document.querySelector('.btn-left')
+const rightBtn = document.querySelector('.btn-right')
+const carouselImages = document.querySelectorAll('.slider-img')
+const carouselWidth = 800
+const carouselSpeed = 3000

a6117189e4b178352b02b8693543703241d23ca4
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-13 17:00:54 +0100

    info and code added

---
+++ js/homes.js
@@ -0,0 +1,13 @@
+const accordion = document.querySelector('.accordion')
+const accordionBtns = document.querySelectorAll('.accordion-btn')
+const test = document.querySelector('.test')
+const btn = document.querySelector('#btn')
+const td = document.getElementsByTagName('td')
+
+//--------------------------------------------------
+
+function openAccoidrionItems() {
+	console.log(this.nextElementSibling.classList.toggle('active'));
+}
+accordionBtns.forEach(btn => btn.addEventListener('click', openAccoidrionItems))
+openAccoidrionItems()

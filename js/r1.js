ec438bf3e4b2a9c86e3be530947f6d3b69336b20
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-15 12:17:43 +0100

    modified

--- js/r1.js
+++ js/r1.js
@@ -1,29 +1,52 @@
-fetch('https://hp-api.herokuapp.com/api/characters/house/ravenclaw')
-	.then((data) => {
-		return data.json();
-	})
-	.then((objectData) => {
-		let favData = '';
-		objectData.map((values) => {
-			for (let i = 0; i < 3; i++) {
-				if (favData == false) {
-					favData += `<div class="card">
-		<img class="image" src="${values.image}"</img>
-		  <div class="container">
-		  <ul>
-		  <li><h2>Name:${values.name}</h2></li>
-		  <li><h2>Actor:${values.actor}</h2></li>
-		  <li><h2>House:${values.house}</h2></li>
-		  <li><h2>Age:${values.dateOfBirth}</h2></li>
-		  <li><h2>Year:${values.yearOfBirth}</h2></li>
-		  <li><h2>Hair:${values.hairColour}</h2></li>
-		  <li><h2>Eyes:${values.eyeColour}</h2></li>
-		  <li><h2>Blood:${values.ancestry}</h2></li>
-		</ul>
-		  </div>
-		</div>`;
-				}
-				document.getElementById('ravenclaw').innerHTML = favData;
-			}
-		});
+let studentArray = [];
+
+async function getStudents(url) {
+	const student = await fetch(url);
+	const jsondata = await student.json();
+	jsondata.forEach((student) => {
+		studentArray.push(student);
 	});
+}
+function listStudents() {
+	const student = document.getElementById('slytherin');
+	for (let i = 0; i < 5; i++) {
+		if (studentArray[i] == false) {
+			student.innerHTML += `<div class="card">
+	  <img class="image" src="${studentArray[0].image}"</img>
+		<div class="container">
+		<ul>
+		<li><h2>Name:${studentArray[i].name}</h2></li>
+		<li><h2>House:${studentArray[i].house}</h2></li>
+		<li><h2>Age:${studentArray[i].yearOfBirth}</h2></li>
+		</ul>
+		</div>
+	  </div>`;
+		} else if (studentArray[i] == '') {
+			student.innerHTML += `<div class="card">
+	  <img class="image" src="${studentArray[i].image}"</img>
+		<div class="container">
+		  <ul>
+		  <li><h2>Name:${studentArray[i].name}</h2></li>
+		  <li><h2>House:${studentArray[i].house}</h2></li>
+		</ul>
+		</div>
+	  </div>`;
+		} else {
+			student.innerHTML += `<div class="card">
+	  <img class="image" src="${studentArray[i].image}"</img>
+		<div class="container">
+		  <ul>
+		  <li><h2>Name:${studentArray[i].name}</h2></li>
+		  <li><h2>House:${studentArray[i].house}</h2></li>
+		  <li><h2>Age:${studentArray[i].yearOfBirth}</h2></li>
+		</ul>
+		</div>
+	  </div>`;
+		}
+	}
+		}
+getStudents('http://hp-api.herokuapp.com/api/characters/house/ravenclaw').then(() => {
+	listStudents();
+})
+
+//-----------------------------------
--- js/s1.js
+++ js/s1.js
@@ -1,37 +1,3 @@
-// let studentArray = [];
-
-// fetch('http://hp-api.herokuapp.com/api/characters/house/slytherin')
-// 	.then((data) => {
-// 		return data.json();
-// 	})
-// 	.then((objectData) => {
-// 		let favData = '';
-// 		objectData.map((values) => {
-// 			for (let i = 0; i < 3; i++) {
-// 				if (favData == false) {
-// 					favData += `<div class="card">
-// 		<img class="image" src="${studentArray.image[0]}"</img>
-// 		  <div class="container">
-// 		  <ul>
-// 		  <li><h2>Name:${values.name}</h2></li>
-// 		  <li><h2>Actor:${values.actor}</h2></li>
-// 		  <li><h2>House:${values.house}</h2></li>
-// 		  <li><h2>Age:${values.dateOfBirth}</h2></li>
-// 		  <li><h2>Year:${values.yearOfBirth}</h2></li>
-// 		  <li><h2>Hair:${values.hairColour}</h2></li>
-// 		  <li><h2>Eyes:${values.eyeColour}</h2></li>
-// 		  <li><h2>Blood:${values.ancestry}</h2></li>
-// 		</ul>
-// 		  </div>
-// 		</div>`;
-// 				}
-// 				document.getElementById('slytherin').innerHTML = favData;
-// 			}
-// 		});
-// 	});
-
-	//----------------
-
 let studentArray = [];
 
 async function getStudents(url) {
@@ -82,3 +48,39 @@ function listStudents() {
 getStudents('http://hp-api.herokuapp.com/api/characters/house/slytherin').then(() => {
 	listStudents();
 })
+
+//---------------------------------
+
+// let studentArray = [];
+
+// fetch('http://hp-api.herokuapp.com/api/characters/house/slytherin')
+// 	.then((data) => {
+// 		return data.json();
+// 	})
+// 	.then((objectData) => {
+// 		let favData = '';
+// 		objectData.map((values) => {
+// 			for (let i = 0; i < 3; i++) {
+// 				if (favData == false) {
+// 					favData += `<div class="card">
+// 		<img class="image" src="${studentArray.image[0]}"</img>
+// 		  <div class="container">
+// 		  <ul>
+// 		  <li><h2>Name:${values.name}</h2></li>
+// 		  <li><h2>Actor:${values.actor}</h2></li>
+// 		  <li><h2>House:${values.house}</h2></li>
+// 		  <li><h2>Age:${values.dateOfBirth}</h2></li>
+// 		  <li><h2>Year:${values.yearOfBirth}</h2></li>
+// 		  <li><h2>Hair:${values.hairColour}</h2></li>
+// 		  <li><h2>Eyes:${values.eyeColour}</h2></li>
+// 		  <li><h2>Blood:${values.ancestry}</h2></li>
+// 		</ul>
+// 		  </div>
+// 		</div>`;
+// 				}
+// 				document.getElementById('slytherin').innerHTML = favData;
+// 			}
+// 		});
+// 	});
+
+	//----------------
